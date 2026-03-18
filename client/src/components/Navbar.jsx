9923a5724b5d5ed9038d5d5241baf0ee81c822eb
Rahul Roy <royrahulroy2004@gmail.com>
2026-03-19 01:04:36 +0530

    minor changes in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             <img src={assets.logo} alt="" className='w-28 sm:w-32' />
             {userData ?
                 <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
-                    {userData.name[0].toUpperCase()}
+                    {userData.name?.[0]?.toUpperCase() ?? '?'}
                     <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
                         <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
                             {!userData.isAccountVerified && <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>}
