476066de3370c4eea5a6ebaf11386102723a739e
Rahul Roy <royrahulroy2004@gmail.com>
2026-03-19 01:44:05 +0530

    fix: vercel SPA routing, cross-domain cookie settings

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,8 +29,8 @@ export const register = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 3600 * 1000
         });
 
@@ -75,8 +75,8 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 3600 * 1000
         });
 
@@ -91,8 +91,8 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
         })
 
         return res.json({ success: true, message: "Logged Out" });
